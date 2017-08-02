aa752e8b0a150561658ccf50bb6b53de9b64e5b2
HellaBored <leecombs_30@hotmail.com>
2017-08-02 12:47:59 -0700

    Activation test and refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,10 +35,21 @@ class User < ApplicationRecord
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-  
+
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   private
