26748cc3429c29c1349eb493a567e8f741cce461
HellaBored <leecombs_30@hotmail.com>
2017-08-03 12:29:57 -0700

    Microposts ordered by creation date, are destroyed when associated user is destroyed

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) } # Ensure descending creation order by default
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
